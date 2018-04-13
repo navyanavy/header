6e9d79ecffb1b84d403cd2c73884ec618835f714
Navya B <navyabachati@gmail.com>
2018-04-12 22:43:26 -0500

    header

---
+++ visaheader/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  menuTitle = [{"name":"Pay With Visa"}, {"name":"Partner With Us"}, {"name":"Run Your Business"},{"name":"Travel With Visa"}, {"name":"Visa Everywhere"}];
+}
